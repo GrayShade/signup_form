67ebcfcef80d117b990f1402c079d689988425b5
Gray <silicongray@gmail.com>
2023-11-07 02:53:26 +0500

    Refactor code to delete unnecessory spaces & comments

--- js.js
+++ js.js
@@ -1,6 +1,4 @@
 function submitForm(e) {
-  console.log(e);
-  // ele = e.target.elements[0]
   fname_check_presence = checkEmptyAfterSubmit(e, 0, 'fname-message');
   email_check_presence = checkEmptyAfterSubmit(e, 2, 'email-message');
   pass_check_presence = checkEmptyAfterSubmit(e, 4, 'pass-message');
@@ -27,13 +25,9 @@ var matchPass = function (event) {
 }
 
 function checkEmptyAfterSubmit(e, index, message) {
-  debugger;
-  // eleName = name.replace(/['"]+/g, '');
   eleVal = e.target.elements[index].value;
-  // let field_name = document.getElementById(name).value;
   let span_message = document.getElementById(message);
   if (eleVal != '') {
-    // message.style.color = 'green';
     span_message.innerHTML = ''
   } else {
     span_message.style.color = 'red';
@@ -50,23 +44,17 @@ function checkEmptyBeforeSubmit(event, eleName, eleMessage) {
     return;
   }
 
+  // When confirm pass field is filled & then pass field is filled afterwards, Password matched message is shown,
+  //  then this function is called & its << message.innerHTML = '' >> overwrites the Password Matched message. So,:
   if (eleVal != '' && (message.innerHTML == 'Passwords Match!' || message.innerHTML == '*Passwords do not Match!')) {
     return;
   }
-  // here is an issue. When confirm pass field is filled & then pass field is filled afterwards, Password matched message is shown,
-  //  then this function is called & its << message.innerHTML = '' >> overwrites the Password Matched message. What to do?
+
   if (eleVal != '') {
-    // message.style.color = 'green';
     message.innerHTML = ''
   }
-  // else  
-  // if (event.key === 'Tab'){
-  //   return;
-  // } 
   else {
     message.style.color = 'red';
     message.innerHTML = "*Field Required!"
   }
 }
-
-function show
